252e6d47986d90282723074d4d13d12dc2ecf16a
Pradyuman Dixit <crvasu779@gmail.com>
2019-10-01 09:46:47 +0200

    Create binarySearch.js

---
+++ binarySearch.js
@@ -0,0 +1,23 @@
+let recursiveFunction = function (arr, x, start, end) { 
+       
+    // Base Condtion 
+    if (start > end) 
+      return -1; 
+   
+    // Find the middle index 
+    let mid = Math.floor((start + end)/2); 
+   
+    // Compare mid with given key x 
+    if (arr[mid] === x) 
+      return mid; 
+          
+    // If element at mid is greater than x, 
+    // search in the left half of mid 
+    if(arr[mid] > x)  
+        return recursiveFunction(arr, x, start, mid-1); 
+    else{
+        // If element at mid is smaller than x, 
+        // search in the right half of mid 
+        return recursiveFunction(arr, x, mid+1, end); 
+    }
+} 
