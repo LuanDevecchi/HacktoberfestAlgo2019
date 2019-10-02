1f38049c3dc7af90d8686157d9283a097f0cbf33
TriGaucho <diogo254@gmail.com>
2019-10-02 17:25:09 -0300

    Minha contribuição para Hacktoberfest

---
+++ caesar_cipher.js
@@ -0,0 +1,27 @@
+let ceaserCipher = (str) => {
+    //Deciphered reference letters
+    let decoded = {
+      a: 'n', b: 'o', c: 'p',
+      d: 'q', e: 'r', f: 's',
+      g: 't', h: 'u', i: 'v',
+      j: 'w', k: 'x', l: 'y',
+      m: 'z', n: 'a', o: 'b',
+      p: 'c', q: 'd', r: 'e',
+      s: 'f', t: 'g', u: 'h',
+      v: 'i', w: 'j', x: 'k',
+      y: 'l', z: 'm'    
+    }
+    
+    //convert the string to lowercase
+    str = str.toLowerCase();
+    
+    //decipher the code
+    let decipher = '';
+    for(let i = 0 ; i < str.length; i++){
+      decipher += decoded[str[i]];
+    }
+    
+    //return the output
+    return decipher;
+    
+  }
