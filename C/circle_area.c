b186d04babf176b5b1c58ca0991bf177c4adfb66
kanimesha99 <90630567+kanimesha99@users.noreply.github.com>
2021-10-03 06:29:35 -0700

    add the main function

--- C/circle_area.c
+++ C/circle_area.c
@@ -1,3 +1,15 @@
+#include<stdio.h>
+float circle_area(float radius);
+void main()
+{
+    float r;
+    float area=0;
+    printf("Enter the radius of circle:");
+    scanf("%f",&r);
+    area=circle_area(r);
+    printf("The area is:%.2f",area);
+}
+
 float circle_area(float radius)
 {
   return (radius * radius) * 3.1415;
